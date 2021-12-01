be567f963a5d800f6b0292137d8532f09b35d673
emacsnt <emacsnt@gmail.com>
2021-12-01 13:31:23 +0800

    Update diy-part2.sh
    remove custom feeds

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,11 +9,11 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/application/luci-app-smartdns
-rm -rf feeds/kenzo/smartdns
-rm -rf feeds/kenzo/luci-app-smartdns
-git clone https://github.com/lisakhan/openwrt-smartdns feeds/kenzo/smartdns
+#rm -rf feeds/packages/net/smartdns
+#rm -rf feeds/luci/application/luci-app-smartdns
+#rm -rf feeds/kenzo/smartdns
+#rm -rf feeds/kenzo/luci-app-smartdns
+#git clone https://github.com/lisakhan/openwrt-smartdns feeds/kenzo/smartdns
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
