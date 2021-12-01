00676c42900682c148270e38d8bbffaa046f4905
emacsnt <emacsnt@gmail.com>
2021-12-01 11:23:13 +0800

    Update diy-part2.sh
    remove duplicate smartdns

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,9 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/kenzo/smartdns
+rm -rf feeds/kenzo/luci-app-smartdns
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
