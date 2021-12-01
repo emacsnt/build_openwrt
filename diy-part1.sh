8cf2ed23bf78903b8ecfa267112de7bee4e5b32f
emacsnt <emacsnt@gmail.com>
2021-12-01 11:42:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,6 @@
 
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git lisa_smart https://github.com/lisakhan/openwrt-smartdns' >>feeds.conf.default
 
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
