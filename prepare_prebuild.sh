a055d7107b0e82835f3231899a4634d7ecf7e6ab
emacsnt <emacsnt@gmail.com>
2021-12-04 13:19:16 +0800

    Create prepare_prebuild.sh
    get prebuild file and prepare

---
+++ prepare_prebuild.sh
@@ -0,0 +1,4 @@
+wget https://github.com/emacsnt/build_openwrt/releases/download/prebuild/prebuild_src.tar.gz
+tar xfz prebuild_src.tar.gz
+wget https://github.com/emacsnt/build_openwrt/releases/download/prebuild/prebuild_cache.tar.gz
+tar xfz prebuild_cache.tar.gz
