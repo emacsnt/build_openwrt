63f505f9598e87bc36c037544a207c4017fc0bcc
emacsnt <emacsnt@gmail.com>
2021-12-04 19:47:15 +0800

    Update diy-part1.sh
    add upx repo

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,5 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git upx-repo https://github.com/kuoruan/openwrt-upx' >>feeds.conf.default
